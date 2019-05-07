23c8b97a263ad9a2ebc87edd275c112568398cd4
unknown <william.pfaffe@gmail.com>
2019-05-07 15:07:00 +0200

    Ejected from react-scripts and rewritten custom boilerplate

--- src/__tests__/api.test.ts
+++
@@ -1,8 +0,0 @@
-import Api from '../api/Api';
-
-describe("Testing API Endpoint", () => {
-    test("Testing POST Request", async () => {
-        const serverResponse = await Api.testServer();
-        expect(serverResponse).toBe({sut: "sut"});
-    })
-})
